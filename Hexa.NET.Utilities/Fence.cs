6e19457265131ee4ce3c4adde41b617f8ca9e0d9
Juna <46632782+JunaMeinhold@users.noreply.github.com>
2024-08-13 18:37:19 +0200

    Added Fence and SemaphoreLight.

---
+++ Hexa.NET.Utilities/Fence.cs
@@ -0,0 +1,54 @@
+﻿namespace Hexa.NET.Utilities
+{
+    using System.Diagnostics;
+    using System.Threading;
+
+    public struct Fence
+    {
+        private int _state;
+
+        public Fence()
+        {
+            _state = 0;
+        }
+
+        public void Signal()
+        {
+            Interlocked.Exchange(ref _state, 1);
+        }
+
+        public void Wait()
+        {
+            while (Interlocked.CompareExchange(ref _state, 0, 0) == 0)
+            {
+                Thread.Yield();
+            }
+        }
+
+        public bool Wait(int timeout)
+        {
+            if (timeout == 0)
+            {
+                return Interlocked.CompareExchange(ref _state, 0, 0) != 0;
+            }
+
+            var end = Stopwatch.GetTimestamp() + timeout * (Stopwatch.Frequency / 1000);
+            while (Interlocked.CompareExchange(ref _state, 0, 0) == 0)
+            {
+                var now = Stopwatch.GetTimestamp();
+                if (now >= end)
+                {
+                    return false;
+                }
+
+                Thread.Yield();
+            }
+            return true;
+        }
+
+        public void Reset()
+        {
+            Interlocked.Exchange(ref _state, 0);
+        }
+    }
+}
---
+++ Hexa.NET.Utilities/SemaphoreLight.cs
@@ -0,0 +1,68 @@
+﻿namespace Hexa.NET.Utilities
+{
+    using System.Runtime.CompilerServices;
+
+    public struct SemaphoreLight
+    {
+        private int currentCount;
+        private readonly int maxCount;
+        private int spinLock;
+
+        public SemaphoreLight(int initialCount, int maxCount)
+        {
+            if (initialCount > maxCount || initialCount < 0 || maxCount <= 0)
+            {
+                throw new ArgumentException("Invalid semaphore initial or max count");
+            }
+
+            currentCount = initialCount;
+            this.maxCount = maxCount;
+            spinLock = 0;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void AcquireLock()
+        {
+            while (Interlocked.CompareExchange(ref spinLock, 1, 0) != 0)
+            {
+                Thread.Yield(); // Minimiert CPU-Belastung, aber ist noch kein Spin-Waiting
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void ReleaseLock()
+        {
+            Volatile.Write(ref spinLock, 0);
+        }
+
+        public void Wait()
+        {
+            while (true)
+            {
+                AcquireLock();
+                if (currentCount > 0)
+                {
+                    currentCount--;
+                    ReleaseLock();
+                    break;
+                }
+                ReleaseLock();
+                Thread.Sleep(1); // Minimale Blockierung, um CPU zu entlasten, kein Spin-Waiting
+            }
+        }
+
+        public void Release()
+        {
+            AcquireLock();
+            if (currentCount < maxCount)
+            {
+                currentCount++;
+            }
+            else
+            {
+                throw new InvalidOperationException("Semaphore released too many times");
+            }
+            ReleaseLock();
+        }
+    }
+}
